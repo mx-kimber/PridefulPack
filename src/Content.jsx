3e2a0e09457355ca5caebaafd15aa398aa95f03b
Kimber Ogden <mx.kimber@gmail.com>
2023-09-14 22:39:17 -0500

    Create Photos component and set hooks in Content / handle IndexPhotos

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,19 @@
 import axios from "axios";
 import {useState, useEffect } from "react";
 import { AdminCommentsIndex } from "./AdminCommentsIndex";
-
+import { PhotosIndex } from "./PhotosIndex";
 export function Content() {
 
   const [adminComments, setAdminComments] = useState([]);
+  const [photos, setPhotos] = useState([]);
+
+  const handleIndexPhotos = () => {
+    console.log("handleIndexPhotos");
+    axios.get("http://localhost:3000/photos.json").then((response) => {
+      console.log(response.data);
+      setPhotos(response.data);
+    });
+  };
 
   const handleIndexAdminComments = () => {
     console.log("handleIndexAdminComments");
@@ -14,11 +23,15 @@ export function Content() {
     });
   };
 
-  useEffect(handleIndexAdminComments, []);
-
+  useEffect(() => {
+    handleIndexAdminComments();
+    handleIndexPhotos();
+  }, []);
+  
   return (
     <div>
       <AdminCommentsIndex adminComments={adminComments} />
+      <PhotosIndex photos={photos} />
     </div>
   )
 }
---
+++ src/PhotosIndex.jsx
@@ -0,0 +1,19 @@
+export function PhotosIndex(props) {
+  return (
+    <div className="container-wrapper">
+      <div className="photos-index-container">
+        {props.photos.map((photo) => (
+          <div key={photo.id} className="photo-item">
+            {/* <h2>{photo.pet_name}</h2> */}
+            <img
+              src={photo.pet_photo}
+              alt={photo.pet_name} 
+              className="pet-photo" 
+            />
+            {/* <p>{photo.caption}</p> */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
