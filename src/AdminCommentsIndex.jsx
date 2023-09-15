1145e029385cf47183edefc211429a7168826029
Kimber Ogden <mx.kimber@gmail.com>
2023-09-14 23:37:36 -0500

    Add associated reviews to admin comments index

--- src/AdminCommentsIndex.jsx
+++ src/AdminCommentsIndex.jsx
@@ -2,14 +2,21 @@ export function AdminCommentsIndex(props) {
   return (
     <div>
       <h1>All admin comments</h1>
-        {props.adminComments.map((adminComment) => (
-         <div key={adminComment.id}>
-           <p>{adminComment.review_id}</p>
-           <p>{adminComment.user_id}</p>
-           <p>{adminComment.comment}</p>
-           <p>{adminComment.created_at}</p>
-         </div>
-       ))}
+      {props.adminComments.map((adminComment) => (
+        <div key={adminComment.id}>
+          <p>{adminComment.comment}</p>
+          <p>{adminComment.created_at}</p>
+          {adminComment.review && (
+            <div>
+              <h2>Associated Review</h2>
+              <p>Rating: {adminComment.review.rating}</p>
+              <p>Comment: {adminComment.review.comment}</p>
+              <p>Name: {adminComment.review.reviewer.name}</p>
+              <p>Created At: {adminComment.review.created_at}</p>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
