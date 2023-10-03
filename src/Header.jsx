30d3a792052dffccab49f7fc8f46a1a555672e48
Kimber Ogden <mx.kimber@gmail.com>
2023-10-02 21:11:52 -0500

    Add active button highlighting functionality to nav buttons. Add active button styling, modify CSS

--- src/Header.jsx
+++ src/Header.jsx
@@ -14,24 +14,53 @@ export function Header() {
 
   return (
     <header>
-      <div className="header">
-        <div className="nav-bar">
-          <button className="nav-button" 
-            onClick={() => window.location.href = "PridefulPack"}>Home</button>
-          <button className="nav-button" onClick={() => window.location.href = "gallery"}>Gallery</button>
-          <button className="nav-button" onClick={() => window.location.href = "services"}>Services</button>
-          <button className="nav-button" onClick={() => window.location.href = "reviews"}>Reviews</button>
-          <button className="nav-button" onClick={() => window.location.href = "about"}>About</button>
-          
+      <div>
+        <div>
+          <button
+            className={`nav-button ${window.location.href.endsWith("PridefulPack") ? 'active' : ''}`}
+            onClick={() => window.location.href = "PridefulPack"}
+          >
+            Home
+          </button>
+          <button
+            className={`nav-button ${window.location.href.endsWith("gallery") ? 'active' : ''}`}
+            onClick={() => window.location.href = "gallery"}
+          >
+            Gallery
+          </button>
+          <button
+            className={`nav-button ${window.location.href.endsWith("services") ? 'active' : ''}`}
+            onClick={() => window.location.href = "services"}
+          >
+            Services
+          </button>
+          <button
+            className={`nav-button ${window.location.href.endsWith("reviews") ? 'active' : ''}`}
+            onClick={() => window.location.href = "reviews"}
+          >
+            Reviews
+          </button>
+          <button
+            className={`nav-button ${window.location.href.endsWith("about") ? 'active' : ''}`}
+            onClick={() => window.location.href = "about"}
+          >
+            About
+          </button>
+
           {currentUser ? (
-            <div className="dropdown admin-dropdown">
+            <div>
               <div>
                 <img className="profile-photo" src={currentUser.profile_photo} alt="User Profile" onClick={toggleDropdown} />
               </div>
               {showDropdown && (
                 <div className="dropdown-content">
                   {currentUser ? `Welcome, ${currentUser.first_name}!` : null}
-                  <button className="nav-button" onClick={() => window.location.href = "admin_dashboard"}>Dashboard</button>
+                  <button
+                    className={`nav-button ${window.location.href.endsWith("admin_dashboard") ? 'active' : ''}`}
+                    onClick={() => window.location.href = "admin_dashboard"}
+                  >
+                    Dashboard
+                  </button>
                   {/* Add a message of how many reviews are new. Make logic for counting reviews with no admin comments */}
                   <button className="nav-button">
                     <LogoutLink />
