2c3aeec1cf760c4b0119f96731a5798e3ea6ad00
Kimber Ogden <mx.kimber@gmail.com>
2023-10-03 23:50:40 -0500

    Fix: undo deletion of LogoutLink component, delete Logout.css file.

---
+++ src/LogoutLink.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import axios from "axios";
+import { UserContext } from "./UserContext";
+import "./Header.css";
+
+export function LogoutLink() {
+  const { setCurrentUser } = useContext(UserContext);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    setCurrentUser(null);
+    window.location.href = "/admin_login";
+  };
+
+  return (
+    <a href="/logout" onClick={handleClick} className="logout-link-btn">
+      Logout
+    </a>
+  );
+}
