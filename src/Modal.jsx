8ab5e57fc49c7e159bd079ace50b7a0e95d64ff0
Kimber Ogden <mx.kimber@gmail.com>
2023-10-05 13:46:52 -0500

    Add Modal componenet with CSS, adjust margins/width

---
+++ src/Modal.jsx
@@ -0,0 +1,16 @@
+import "./Modal.css";
+
+export function Modal(props) {
+  if (props.show) {
+    return (
+      <div className="modal-background">
+        <section className="modal-main">
+          {props.children}
+          <button className="close" type="button" onClick={props.onClose}>
+            &#x2715;
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
