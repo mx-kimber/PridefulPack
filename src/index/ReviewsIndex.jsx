389b5369614c763f322b0168dc6ba98c1bd179d1
Kimber Ogden <mx.kimber@gmail.com>
2023-10-14 21:24:13 -0500

    Implement a star system for review ratings, use star unicode

--- src/index/ReviewsIndex.jsx
+++ src/index/ReviewsIndex.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
 export function ReviewsIndex(props) {
+  const renderStars = (rating) => {
+    const maxRating = 5;
+    const stars = [];
+    for (let i = 1; i <= maxRating; i++) {
+      const starClassName = i <= rating ? 'star filled' : 'star';
+      stars.push(<span key={i} className={starClassName}>&#9733;</span>);
+    }
+    return stars;
+  };
+
   return (
     <div className="main-container">
       <h1>Reviews</h1>
       {props.reviews.map((review) => (
         <div key={review.id}>
-          <p>Rating: {review.rating}</p>
+          <p>Rating: {renderStars(review.rating)}</p>
           <p>{review.comment}</p>
           <p>{review.reviewer.name}</p>
           <p>{review.created_at}</p>
