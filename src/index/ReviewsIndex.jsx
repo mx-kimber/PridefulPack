9015ad28a4e17332f849afa45e82ec70d9b3a5d3
Kimber Ogden <mx.kimber@gmail.com>
2023-10-14 23:31:19 -0500

    Create containers and restructure reviews index, add CSS for a card style layout

--- src/index/ReviewsIndex.jsx
+++ src/index/ReviewsIndex.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./CSS/ReviewsIndex.css"
 
 export function ReviewsIndex(props) {
   const renderStars = (rating) => {
@@ -10,23 +11,40 @@ export function ReviewsIndex(props) {
   };
 
   return (
-    <div className="main-container">
+    <div>
       <h1>Reviews</h1>
       {props.reviews.map((review) => (
         <div key={review.id}>
-          <p>Rating: {renderStars(review.rating)}</p>
-          <p>{review.comment}</p>
-          <p>{review.reviewer.name}</p>
-          <p>{review.created_at}</p>
-
+          <div className="review">
+            <div className="reviewer-photo-container">
+              <img className="reviewer-photo" src={review.reviewer.profilePhotoUrl} alt="Profile" />
+            </div>
+            <div className="rating">
+              {renderStars(review.rating)}
+            </div>
+            <div className="comment">
+              <p>{review.comment}</p>
+            </div>
+            <div className="review-details-name">
+              <p>- {review.reviewer.name}</p>
+            </div>
+          </div>
           {review.admin_comments && (
-            <div>
+            <div className="admin-comments">
               <h3>Associated Admin Comments</h3>
               {review.admin_comments.map((adminComment) => (
                 <div key={adminComment.id}>
-                  <p>{adminComment.comment}</p>
-                  <p>{adminComment.user.full_name}</p>
-                  <p>{adminComment.user.created_at}</p>
+                  <div className="review">
+                    <div className="reviewer-photo-container">
+                      <img className="reviewer-photo" src={adminComment.user.profilePhotoUrl} alt="Profile" />
+                    </div>
+                    <div className="review-details-name">
+                      <p>- {adminComment.user.full_name}</p>
+                    </div>
+                  </div>
+                  <div className="comment">
+                    <p>{adminComment.comment}</p>
+                  </div>
                 </div>
               ))}
             </div>
