9aa49e98d9d790e42b33142a7a6bf967b6bacbaa
Kimber Ogden <mx.kimber@gmail.com>
2023-11-04 10:46:25 -0500

    Show upload feature in footer only when currentUser is logged in

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { Modal } from "./Modal";
 // import { UploadForm } from "./UploadForm"
 import { UploadImages } from "./UploadImages";
-// import { ImageUploader } from './ImageUploader';
+import { UserContext } from './UserContext';
 
 export function Footer() {
   const [isImagesUploadVisible, setIsImagesUploadVisible] = useState(false);
-  
+  const { currentUser, setCurrentUser } = useContext(UserContext);
+
   const handleClose = () => {
     setIsImagesUploadVisible(false);
   };
@@ -19,19 +20,23 @@ export function Footer() {
  
   return (
     <footer>
-      <FontAwesomeIcon
-        icon={faCamera}
-        size="lg"
-        className="camera-icon"
-        onClick={handleOpenImagesUpload}
-      />
-      <Modal
-        show={isImagesUploadVisible}
-        onClose={handleClose}>
-        {/* <UploadForm /> */}
+    {currentUser ? (
+      <>
+        <FontAwesomeIcon
+          icon={faCamera}
+          size="lg"
+          className="camera-icon"
+          onClick={handleOpenImagesUpload}
+        />
+        <Modal
+          show={isImagesUploadVisible}
+          onClose={handleClose}
+        >
           <UploadImages />
-        {/* <ImageUploader /> */}
-      </Modal>
+          {/* <UploadForm /> */}
+        </Modal>
+      </>
+    ) : null}
     </footer>
   );
 }
