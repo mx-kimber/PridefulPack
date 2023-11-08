78ec2f5a571286611859d9ac53df669d9843627a
Kimber Ogden <mx.kimber@gmail.com>
2023-11-07 22:11:21 -0600

    Add navigation buttons to AdminSidebar  with functionality to hide sidebar and buttons when retracted

--- src/AdminSidebar.jsx
+++ src/AdminSidebar.jsx
@@ -2,18 +2,28 @@ import React, { useContext } from 'react';
 import { UserContext } from './UserContext';
 import './AdminSidebar.css';
 
-export function AdminSidebar() {
+export function AdminSidebar({ isRetracted }) {
   const { currentUser } = useContext(UserContext);
 
+  const generateNavButton = (text, url) => {
+    return (
+      <button className="sidebar-button" onClick={() => window.location.href = url}>
+        {text}
+      </button>
+    );
+  };
+
   if (!currentUser) {
-    return null; 
+    return null;
   }
 
   return (
-    <div className="sidebar-container">
-      links <br />
-      links <br />
-      links
+    <div className={`sidebar-container ${isRetracted ? 'retracted' : ''}`}>
+      {generateNavButton('Photo Gallery', '/admin_gallery')}
+      {generateNavButton('Update Contact', '/admin_contact')}
+      {generateNavButton('Edit Services', '/admin_service_offerings')}
+      {generateNavButton('Edit Bio', '/admin_bio')}
+     
     </div>
   );
 }
