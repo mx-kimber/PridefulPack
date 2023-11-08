05a77c45a5fc564ed76cf8ba01a944d9601ae0a3
Kimber Ogden <mx.kimber@gmail.com>
2023-11-08 00:39:19 -0600

    Refactor AdminSidebar component for dynamic sidebar retraction based on viewport width

--- src/AdminSidebar.jsx
+++ src/AdminSidebar.jsx
@@ -1,16 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from './UserContext';
 import './AdminSidebar.css';
 
-export function AdminSidebar({ isRetracted }) {
+export function AdminSidebar() {
   const { currentUser } = useContext(UserContext);
+  const [isRetracted, setIsRetracted] = useState(window.innerWidth < 970);
+
+  useEffect(() => {
+    function handleResize() {
+      setIsRetracted(window.innerWidth < 970);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const generateNavButton = (text, url) => {
     return (
-      <button className="sidebar-button" onClick={() => window.location.href = url}>
+      <button
+        className={`sidebar-button ${isRetracted ? 'retracted' : ''}`}
+        onClick={() => {
+          if (!isRetracted) {
+            window.location.href = url;
+          }
+        }}
+      >
         {text}
       </button>
     );
+  };
+
+  const toggleSidebar = () => {
+    setIsRetracted(!isRetracted);
   };
 
   if (!currentUser) {
@@ -18,14 +41,12 @@ export function AdminSidebar({ isRetracted }) {
   }
 
   return (
-    <div className={`sidebar-container ${isRetracted ? 'retracted' : ''}`}>
+    <div className={`sidebar-container ${isRetracted ? 'retracted' : ''}`} onClick={toggleSidebar}>
       {generateNavButton('Photo Gallery', '/admin_gallery')}
       {generateNavButton('Update Contact', '/admin_contact')}
       {generateNavButton('Edit Services', '/admin_service_offerings')}
       {generateNavButton('Edit Bio', '/admin_bio')}
-     
     </div>
   );
 }
 
-export default AdminSidebar;
