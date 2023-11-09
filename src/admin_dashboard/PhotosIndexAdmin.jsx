1a7858274c5a2cf1ec808f2651e73ac862ab5701
Kimber Ogden <mx.kimber@gmail.com>
2023-11-08 20:30:38 -0600

    Enhance PhotosIndexAdmin component for better user interaction / add edit button and style to fit with the admin-pet-gallery container

--- src/admin_dashboard/PhotosIndexAdmin.jsx
+++ src/admin_dashboard/PhotosIndexAdmin.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons'; // Import the edit icon
 import "./AdminGallery.css";
 
 export function PhotosIndexAdmin(props) {
@@ -8,18 +11,19 @@ export function PhotosIndexAdmin(props) {
 
   return (
     <div className="admin-pet-gallery">
-      
-        {props.photos.map((photo) => (
-          <div key={photo.id}>
-            <img
-              src={photo.image_url}
-              alt={photo.pet_name}
-              className="pet-index-image"
-              onClick={() => handleImageClick(photo)}
-            />
-          </div>
-        ))}
-    </div>
 
+      {props.photos.map((photo) => (
+        <div key={photo.id}>
+          <img
+            src={photo.image_url}
+            alt={photo.pet_name}
+            className="pet-index-image"
+          />
+          <div className="admin-pet-footer" onClick={() => handleImageClick(photo)}>
+            <FontAwesomeIcon icon={faPencilAlt} />
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
