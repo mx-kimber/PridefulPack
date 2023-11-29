4a92152351db1bcf38e00c3dbc9bc8c3a61407e0
Kimber Ogden <mx.kimber@gmail.com>
2023-11-28 21:45:41 -0600

    Add upload functionality with an image preview

--- src/AvatarUpload.jsx
+++ src/AvatarUpload.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 export function AvatarUpload() {
 
-  const uploadImage = (files) => {
-    console.log(files[0]);
-  }
+  const [imageSelected, setImageSelected] = useState(null);
+  const [cloudinaryUrl, setCloudinaryUrl] = useState(null);
+
+  const handleImageChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setImageSelected(selectedFile);
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setCloudinaryUrl(reader.result);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  const uploadImage = async () => {
+    const formData = new FormData();
+    formData.append("file", imageSelected);
+
+    try {
+      const response = await axios.post("http://localhost:3000/avatar_upload.json", formData);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <div>
-        <h2>
-          Implementing Cloudinary
-        </h2>
+        <h2>Implementing Cloudinary</h2>
+      
       </div>
-      <div className="input">
-        <input 
+      <div>
+        <input
           type="file"
-          onChange={(event) => {
-            uploadImage(event.target.files);
-          }}
+          onChange={handleImageChange}
         />
+        <button onClick={uploadImage}> Upload Image </button>
+      </div>
+      <div>
+        {cloudinaryUrl && (
+          <img 
+            src={cloudinaryUrl} 
+            alt="Preview"
+          />
+        )}
       </div>
     </div>
   )
