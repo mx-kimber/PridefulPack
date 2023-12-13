34c2bf2801d559ba023afd0a7e93aa2869fec9bf
Kimber Ogden <mx.kimber@gmail.com>
2023-12-12 21:01:35 -0600

    [ still styling profile settings route ]

--- src/UsersShow.jsx
+++ src/UsersShow.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { UserContext } from './UserContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserPen } from '@fortawesome/free-solid-svg-icons';
 import './UsersShow.css';
-import { AvatarUpload } from "./AvatarUpload";
+import { AvatarUpload } from './AvatarUpload';
 
 export function UsersShow() {
   const { currentUser } = useContext(UserContext);
@@ -9,8 +11,9 @@ export function UsersShow() {
   return (
     <div>
       <div className="show-heading">
-        User Information</div>
-        
+        User Information
+      </div>
+      
       <div className="show-user-name">
         {currentUser ? `${currentUser.first_name} ${currentUser.last_name}` : null}
       </div>
@@ -19,11 +22,7 @@ export function UsersShow() {
         
         <div className="col-1">
           {currentUser && (
-            
-             <div className="show-photo-wrap">
-                {/* <AvatarUpload /> */}
-              
-              
+            <div className="show-photo-wrap">
               {currentUser.profile_photo && (
                 <img
                   src={`https://res.cloudinary.com/pawparazzi-media/image/upload/${currentUser.profile_photo}`}
@@ -31,49 +30,53 @@ export function UsersShow() {
                   className="show-user-profile-photo"
                 />
               )}
-              <div className="change-profile-photo-button">
-                <AvatarUpload />
+              <div className="edit-icon">
+                <FontAwesomeIcon icon={faUserPen} />
+                  {/* NOTE!!! onClick={openEditProfilePhotoModal} */}
+                
               </div>
             </div> 
-          )}<div className="social-media-wrap">
-          Instagram: {currentUser ? currentUser.instagram : null}<br />
-          Facebook: {currentUser ? currentUser.facebook : null}<br />
-          Note: add into user table in rails
-        </div>
-        <div className="show-info-wrap"> 
-          
-          <div className="name-wrap">
-            Name: {currentUser ? `${currentUser.first_name} ${currentUser.last_name}` : null}
+          )}
+          <div className="social-media-wrap">
+            Instagram: {currentUser ? currentUser.instagram : null}<br />
+            Facebook: {currentUser ? currentUser.facebook : null}<br />
+            Note: add into user table in rails
           </div> 
-          <div className="email-wrap">
-            Email: {currentUser ? currentUser.email : null}
+           <div className="bio-wrap">
+            Bio: {currentUser ? currentUser.bio : null}
           </div>
-          <div className="phone-wrap">
-            Phone Number: {currentUser ? currentUser.phone_number : null}
-          </div>
-          <div className="address-wrap">
-            Address: {currentUser ? currentUser.address : null}
-          </div>
-        </div>
-        </div>
-        <div className="bio-wrap">
-          Bio: {currentUser ? currentUser.bio : null}
-        </div>
-        
-      </div>   
-       
-      {/* <div className="bottom-container">
-        <div className="bio-wrap">
-          Bio: {currentUser ? currentUser.bio : null}
-        </div>
-        
-          </div>
-          <div>
+        </div>     
+          <div className='col-2'>
+            <div className="show-info-wrap"> 
+              <div className="name-wrap">
+                Name: {currentUser ? `${currentUser.first_name} ${currentUser.last_name}` : null}
+              </div> 
+              <div className="email-wrap">
+                Email: {currentUser ? currentUser.email : null}
+              </div>
+              <div className="phone-wrap">
+                Phone Number: {currentUser ? currentUser.phone_number : null}
+              </div>
+              <div className="address-wrap">
+                Address: {currentUser ? currentUser.address : null}
+              </div>
+            </div>
+            <div className='two-cols-one-wrap'>
+                <div className="some-content">
+                Bio: {currentUser ? currentUser.bio : null}
+                </div>
+                <div className="more-content">
+                Email: {currentUser ? currentUser.email : null}
+              </div>
+            </div>
+            <div className="bottom-box">
             <button className="delete-button">
               Delete Account
             </button>
-          </div> */}
+          </div>
         </div>
+      </div>       
+    </div>
   );
 }
 
