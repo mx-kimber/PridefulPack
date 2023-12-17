88d6ebfc09afabc9f9e03bfbcfbbe2e5310394d8
Kimber Ogden <mx.kimber@gmail.com>
2023-12-17 00:32:49 -0600

    Display a blank card in the beginning of the users index map with an 'add user' icon

--- src/index/UsersIndex.jsx
+++ src/index/UsersIndex.jsx
@@ -1,10 +1,10 @@
-import { useContext } from 'react';
+import React, { useContext } from 'react';
 import { UserContext } from '../UserContext';
 import "./UsersIndex.css";
 import axios from 'axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
-import "../buttons.css"
+import { faEyeSlash, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import "../buttons.css";
 
 export function UsersIndex(props) {
   const { currentUser, setCurrentUser } = useContext(UserContext);
@@ -22,42 +22,49 @@ export function UsersIndex(props) {
       <div className="heading">
         Profile Settings
       </div>
-      <div className="user-card-wrap">
-        <div className="user-cards">
-          {props.users.map((user) => (
-            <div
-              key={user.id}
-              className={`card-item ${user.id === currentUser?.id ? 'logged-in' : ''}`}
-            >
-              <div>
-                {user.profile_photo && (
-                  <img
-                    className="user-profile-photo"
-                    src={`https://res.cloudinary.com/pawparazzi-media/image/upload/${user.profile_photo}`}
-                    alt="Profile"
-                  />
-                )}
-              </div>
-              <div className="user-name">
-                {user.first_name} {user.last_name}
-              </div> 
-              {user.id === currentUser?.id ? null : (
-                <div>
-                  <FontAwesomeIcon icon={faEyeSlash} />
-                  <div>
-                    <button className="user-login-button" onClick={() =>  props.openLoginModal()}  >
-                      Log in
-                    </button>
-                  </div>
-                </div>
-              )}
-              <div>
-                {user.id === currentUser?.id && (
-                  <button  className="user-logout-button" onClick={handleLogoutClick}>Logout</button>
-                )}
-              </div>
+      <div className="add-user-card"> 
+        <div className="user-card-wrap">
+          <div className="user-cards">
+            <div className="card-item">
+              <FontAwesomeIcon icon={faUserPlus} />
             </div>
-          ))}
+            {props.users.map((user, index) => (
+              <div
+                key={user.id}
+                className={`card-item ${user.id === currentUser?.id ? 'logged-in' : ''}`}
+              >
+                <div>
+                  {user.profile_photo && (
+                    <img
+                      className="user-profile-photo"
+                      src={`https://res.cloudinary.com/pawparazzi-media/image/upload/${user.profile_photo}`}
+                      alt="Profile"
+                    />
+                  )}
+                </div>
+                <div className="user-name">
+                  {user.first_name} {user.last_name}
+                </div>
+                {user.id === currentUser?.id ? null : (
+                  <div>
+                    <FontAwesomeIcon icon={faEyeSlash} />
+                    <div>
+                      <button className="user-login-button" onClick={() => props.openLoginModal()}>
+                        Log in
+                      </button>
+                    </div>
+                  </div>
+                )}
+                <div>
+                  {user.id === currentUser?.id && (
+                    <button className="user-logout-button" onClick={handleLogoutClick}>
+                      Logout
+                    </button>
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
