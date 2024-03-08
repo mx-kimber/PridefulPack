ec0c7bfcb5a4530da01dd50defb1fadc21c76123
Kimber Ogden <mx.kimber@gmail.com>
2024-03-08 15:01:59 -0600

    Hardcode image link for landing page main image, update page width

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -8,7 +8,7 @@ export function LandingPage() {
   return (
     <div classname='gap-10px column'>
     <div className='main-image-container'>
-        <img className="main-image" src="https://people.com/thmb/eNcS-7goHtK6-5C6dZ6gbT3AtlQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/Dog-Birthday-Party-tout-052423-8cfd398176ce4a50b707ae8ec4406a12.jpg"/>
+        <img className="main-image" src="https://res.cloudinary.com/pawparazzi-media/image/upload/v1709931231/PridefulPack/LandingPage/Screenshot_2024-03-08_at_2.39.15_PM_nbkufs.png"/>
      </div>
   </div>
   );
